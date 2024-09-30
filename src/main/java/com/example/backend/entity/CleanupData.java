7a40089b266f9b51cbf52b83d719df0554a3d582
Jin Han <120617717+Esther2112@users.noreply.github.com>
2024-10-01 02:25:19 +0900

    CleanupData 필드명 오류 수정

--- src/main/java/com/example/backend/entity/CleanupData.java
+++ src/main/java/com/example/backend/entity/CleanupData.java
@@ -15,12 +15,12 @@ public class CleanupData {
     @JoinColumn(name="member_id")
     private MemberInfo memberId;
     @OneToOne
-    @JoinColumn(name="ovserved_data_id")
+    @JoinColumn(name="observed_data_id")
     private ObservedData observedDataId;
     private String beforeCleanupPicture;
     private String afterCleanupPicture;
     private LocalDateTime cleanupDt;
-    private Integer count50liter;
+    private Integer count_50liter;
     private Integer totalCleanupLitter;
     private Geometry collectionLonlat;
     @ManyToOne
