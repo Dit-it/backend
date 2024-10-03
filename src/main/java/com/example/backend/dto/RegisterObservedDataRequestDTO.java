b5463f5511fcbf092546220980c91060ab484d3b
HongChaa <jun761_1@naver.com>
2024-10-03 09:45:51 +0900

    feat: 조사모드 현장사진 업로드 및 데이터 등록 기능 추가

---
+++ src/main/java/com/example/backend/dto/RegisterObservedDataRequestDTO.java
@@ -0,0 +1,47 @@
+package com.example.backend.dto;
+
+import com.example.backend.handler.ImageHandler;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterObservedDataRequestDTO {
+
+    private String observedDataId;
+    private String memberId;
+    private Integer coastCode;
+    private Integer estimationLiter;
+    MultipartFile observedPicture;
+    private String observedPicturePath;
+    private LocalDateTime observedDt;
+    private String litterTypeCode;
+
+    public RegisterObservedDataRequestDTO(String userId, RegisterObservedDataRequestDTO dto, MultipartFile observedPicture) throws IOException {
+
+        this.observedDataId = generateObservedDataId(dto);
+        this.memberId = userId;
+        this.coastCode = dto.getCoastCode();
+        this.estimationLiter = dto.getEstimationLiter();
+        this.observedPicture = observedPicture;
+        this.observedDt = dto.getObservedDt();
+        this.litterTypeCode = dto.getLitterTypeCode();
+    }
+
+    private String generateObservedDataId(RegisterObservedDataRequestDTO dto) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMddHHmm");
+        return dto.getObservedDt().format(formatter) + String.format("%04d", dto.getCoastCode());   // 연 + 월 + 일 + 시 + 분 + 해안코드  => 조사데이터 일련번호 생성
+    }
+
+
+
+}
--- src/main/java/com/example/backend/entity/ObservedData.java
+++ src/main/java/com/example/backend/entity/ObservedData.java
@@ -4,9 +4,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class ObservedData {
 
