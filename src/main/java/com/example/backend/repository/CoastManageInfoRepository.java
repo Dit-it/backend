037f943425f92d1fb4be3c6ad57811b204b84c81
Jin Han <120617717+Esther2112@users.noreply.github.com>
2024-10-06 05:50:25 +0900

    CoastManageInfoRepository 쿼리수정

--- src/main/java/com/example/backend/repository/CoastManageInfoRepository.java
+++ src/main/java/com/example/backend/repository/CoastManageInfoRepository.java
@@ -12,15 +12,16 @@ import java.util.List;
 @Repository
 public interface CoastManageInfoRepository extends JpaRepository<CoastManageInfo, Integer> {
 
-    @Query(value = "SELECT sigungu_code, coast_code, coast_name, " +
+    @Query(value = "SELECT cmi.sigungu_code, sigungu_name, coast_code, coast_name, " +
             "coastline_len, st_asgeojson(coast_lonlat)::jsonb as coast_lonlat, st_asgeojson(coast_geom)::jsonb as coast_geom " +
-            "FROM public.coast_manage_info " +
-            "WHERE sigungu_code = :sigunguCode", nativeQuery = true)
-    List<CoastManageInfo> findAllBySigunguCode(String sigunguCode);
+            "FROM public.coast_manage_info cmi " +
+            "Join sigungu_info si on si.sigungu_code = cmi.sigungu_code " +
+            "WHERE cmi.sigungu_code = :sigunguCode", nativeQuery = true)
+    List<CoastManagaInfoResponseIntercase> findAllBySigunguCode(String sigunguCode);
 
-    @Query(value = "SELECT sigungu_code, sigungu_name, coast_code, coast_name, " +
+    @Query(value = "SELECT cmi.sigungu_code, sigungu_name, coast_code, coast_name, " +
             "coastline_len, st_asgeojson(coast_lonlat)::jsonb as coast_lonlat, st_asgeojson(coast_geom)::jsonb as coast_geom " +
-            "FROM public.coast_manage_info ", nativeQuery = true)
+            "FROM public.coast_manage_info cmi Join sigungu_info si on si.sigungu_code = cmi.sigungu_code ", nativeQuery = true)
     List<CoastManagaInfoResponseIntercase> findAllCoastInfo();
 
 
